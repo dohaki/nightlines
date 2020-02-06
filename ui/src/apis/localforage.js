fdf642ed42e924fd1c2de6d17a64d6770a95f650
dakingha69 <dongha.kim210@gmail.com>
2020-02-06 16:31:38 +0100

    Add localforage as offline storage

---
+++ ui/src/apis/localforage.js
@@ -0,0 +1,36 @@
+import localforage from "localforage";
+
+/**
+ * 
+ * @param {string} name 
+ */
+export function createInstance(name) {
+  if (!name) {
+    throw new Error("Instance name has to be set");
+  }
+  return localforage.createInstance({
+    name: name.toLowerCase()
+  })
+}
+
+/**
+ * 
+ * @param {*} username 
+ * @returns {Promise<{
+ *  username: string,
+ *  walletData: any,
+ *  zkpKeyPair: {
+ *    publicKey: string,
+ *    privateKey: string,
+ * }}>}
+ */
+export async function getUserByUsername(username = "") {
+  const instance = createInstance(username);
+  return instance.getItem("user");
+}
+
+
+export async function setUser({ username, walletData, zkpKeyPair }) {
+  const instance = createInstance(username);
+  return instance.setItem("user", { username, walletData, zkpKeyPair });
+}
